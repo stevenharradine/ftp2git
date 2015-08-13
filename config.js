950883322d8917a2f55c51cf3f857ca0efabe911
steven harradine <steven.harradine@telus.com>
2015-08-13 00:17:58 -0400

    Downloads zip to tmp

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports.FTP_URL  = "myftp.com"
+module.exports.FTP_USER = "username"
+module.exports.FTP_PASS = "password"
+module.exports.KEY_WORD = "-MyProject"
+
+module.exports.GIT_PATH = "https://github.com/stevenharradine/gitrepo.git"
+module.exports.GIT_KEY  = "Private Key"
---
+++ ftp2git.js
@@ -0,0 +1,61 @@
+var CONFIG        = require("./config"),
+    fs            = require('fs'),
+    Client        = require('ftp'),
+    c             = new Client(),
+    c2            = new Client(),
+    clientOptions = {
+      "host": CONFIG.FTP_URL,
+      "user": CONFIG.FTP_USER,
+      "password": CONFIG.FTP_PASS
+    },
+    latestFolder  = "",
+    pathToZip     = "",
+    year          = "",
+    month         = "",
+    day           = ""
+
+c.on('ready', function() {
+  c.list(function(err, list) {
+    if (err) throw err
+
+    for (l in list) {
+      var current_folder_name = list[l].name;
+
+      if (current_folder_name.indexOf(CONFIG.KEY_WORD) >= 0) {
+        latestFolder = current_folder_name
+      }
+    }
+
+    client2Options = {
+      "host": CONFIG.FTP_URL,
+      "user": CONFIG.FTP_USER,
+      "password": CONFIG.FTP_PASS
+    }
+
+    c2.on ('ready', function () {
+      c2.list(latestFolder, function(err, list) {
+        if (err) throw err
+
+        for (l in list) {
+          var filename = list[l].name
+
+          pathToZip = filename
+        }
+
+        c2.get(latestFolder + "/" + pathToZip, function(err, stream) {
+          if (err) throw err;
+          stream.once('close', function() { c.end(); });
+          stream.pipe(fs.createWriteStream("/tmp/" + latestFolder + ".zip"));
+        })
+      })
+
+      c2.end()
+    })
+
+    c2.connect (client2Options)
+
+    c.end()
+  })
+})
+
+c.connect(clientOptions)
