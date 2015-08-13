790ee371aac1a49652467a183f95a7be61c313af
steven harradine <steven.harradine@telus.com>
2015-08-13 08:54:38 -0400

    refactor add functions

--- ftp2git.js
+++ ftp2git.js
@@ -11,42 +11,70 @@ var CONFIG        = require("./config"),
     latestFolder  = "",
     pathToZip     = ""
 
-c.on('ready', function() {
-  c.list(function(err, list) {
-    if (err) throw err
-
-    for (l in list) {
-      var current_folder_name = list[l].name
-
-      if (current_folder_name.indexOf(CONFIG.KEY_WORD) >= 0) {
-        latestFolder = current_folder_name
-      }
-    }
-
-    c2.on ('ready', function () {
-      c2.list(latestFolder, function(err, list) {
-        if (err) throw err
-
-        for (l in list) {
-          var filename = list[l].name
-
-          pathToZip = filename
-        }
-
-        c2.get(latestFolder + "/" + pathToZip, function(err, stream) {
-          if (err) throw err
-          stream.once('close', function() { c.end() })
-          stream.pipe(fs.createWriteStream("/tmp/" + latestFolder + ".zip"))
-        })
-      })
-
-      c2.end()
-    })
-
-    c2.connect (clientOptions)
-
-    c.end()
-  })
+downloadZipFromFtp (function () {
+  console.log ("done")
 })
 
-c.connect(clientOptions)
+function downloadZipFromFtp (callback) {
+  c.on('ready', function() {
+    c.list(function(err, list) {
+      if (err) throw err
+
+      latestFolder = getLatestFolder (list)
+
+      c2.on ('ready', function () {
+        c2.list(latestFolder, function(err, list) {
+          if (err) throw err
+
+          pathToZip = getPathToZip (list)
+
+          c2.get(latestFolder + "/" + pathToZip, function(err, stream) {
+            if (err) throw err
+
+            stream.once('close', function() {
+              c.end()
+
+              callback()
+            })
+
+            stream.pipe(fs.createWriteStream("/tmp/" + latestFolder + ".zip"))
+          })
+        })
+
+        c2.end()
+      })
+
+      c2.connect (clientOptions)
+
+      c.end()
+    })
+  })
+
+  c.connect(clientOptions)
+}
+
+function getLatestFolder (list) {
+  var latestFolder
+
+  for (l in list) {
+    var current_folder_name = list[l].name
+
+    if (current_folder_name.indexOf(CONFIG.KEY_WORD) >= 0) {
+      latestFolder = current_folder_name
+    }
+  }
+
+  return latestFolder
+}
+
+function getPathToZip (list) {
+  var pathToZip
+
+  for (l in list) {
+    var filename = list[l].name
+
+    pathToZip = filename
+  }
+
+  return pathToZip
+}
