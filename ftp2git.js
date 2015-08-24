b6e7a866ddbcab1886ed989dd94f806263c4b9f4
steven harradine <steven.harradine@telus.com>
2015-08-24 08:59:11 -0400

    testing git add/commit/push

--- ftp2git.js
+++ ftp2git.js
@@ -8,15 +8,88 @@ var CONFIG         = require("./config"),
 
 listPrimaryDirectory (function (latestFolder) {
   listSecondaryDirectory (latestFolder, function (c, pathToZip) {
-    downloadZip (c, pathToZip, function () {
+//    downloadZip (c, pathToZip, function () {
       updateGitFiles (0, function () {
         deleteRepoContents (function () {
           extractZipIntoRepo (function () {
             // TODO: git add . -A && git commit -m "update" && git push
+
+
+
+console.log ("00")
+Git.Repository.open("/tmp/ftp2git_repo")
+.then(function(indexResult) {
+  console.log ("A1")
+    index  = indexResult;
+console.log ("A2")
+    // this file is in the root of the directory and doesn't need a full path
+    index.addByPath('.');
+console.log ("A3")
+    // this will write files to the index
+    index.write();
+console.log ("A4")
+    return index.writeTree();
+
+})/*.then(function(oidResult) {
+console.log ("B")
+    oid = oidResult;
+    return Git.Reference.nameToId(repo, "HEAD");
+
+}).then(function(head) {
+console.log ("C")
+    return repo.getCommit(head);
+
+}).then(function(parent) {
+console.log ("D")
+    author = Git.Signature.now("Author Name", "author@email.com");
+    committer = Git.Signature.now("Commiter Name", "commiter@email.com");
+
+    return repo.createCommit("HEAD", author, committer, "Added the Readme file for theme builder", oid, [parent]);
+}).then(function(commitId) {
+  console.log ("E")
+    return console.log("New Commit: ", commitId);
+})
+
+
+/// PUSH
+.then(function() {
+    return repo.getRemote("origin");
+}).then(function(remoteResult) {
+
+  console.log('remote Loaded');
+  remote = remoteResult;
+
+  remote.setCallbacks({
+      credentials: function(url, userName) {
+          return Git.Cred.sshKeyFromAgent(userName)
+      }
+  });
+
+  console.log('remote Configured');
+  return remote.connect(Git.Enums.DIRECTION.PUSH);
+
+}).then(function() {
+  console.log('remote Connected?', remote.connected())
+
+  return remote.push(
+            ["refs/heads/master:refs/heads/master"],
+            null,
+            repo.defaultSignature(),
+            "Push to master")
+}).then(function() {
+    console.log('remote Pushed!')
+})*/
+.catch(function(reason) {
+    console.log("E"+reason);
+})
+
+
+
+
           })
         })
       })
-    })
+//    })
   })
 })
 
